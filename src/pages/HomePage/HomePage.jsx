b4509869428a7f552a607ad39ec6e99f4ff2237d
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-10-09 19:36:50 +0200

    create HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import css from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div>Homepage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.title}>Campers of your dreams</h1>
+      <p className={css.text}>
+        You can find everything you want in our catalog
+      </p>
+      <Button text="View Now" onClick={() => navigate("/catalog")} />
+    </div>
+  );
 };
 
 export default HomePage;
