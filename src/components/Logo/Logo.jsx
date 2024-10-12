1a631883f87a2425f4a636cecc0cf1191656a4ea
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-10-13 00:35:39 +0200

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import sprite from "../../images/sprite.svg";
 import css from "./Logo.module.css";
 
 const Logo = () => {
   return (
     <>
-      <svg className={css.logo}>
-        <use href={`${sprite}#icon-logo`} />
-      </svg>
+      <Link to="/">
+        <svg className={css.logo}>
+          <use href={`${sprite}#icon-logo`} />
+        </svg>
+      </Link>
     </>
   );
 };
